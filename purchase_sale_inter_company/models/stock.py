685119ccf4d329d5b7654a4d452f635eea7b446f
Boyan Rabchev <boyan@rabchev.com>
2020-04-21 17:59:14 +0300

    econt

--- purchase_sale_inter_company/models/stock.py
+++ purchase_sale_inter_company/models/stock.py
@@ -1,8 +1,9 @@
 # pylint:disable=
 # flake8: noqa: E501
-# pylama:ignore=E501
+# pylama:ignore=E501,C901
 
 from odoo import models, api
+from odoo.exceptions import ValidationError
 
 
 class Picking(models.Model):
@@ -10,40 +11,49 @@ class Picking(models.Model):
 
     @api.multi
     def action_done(self):
-        # TODO: Refactor this moethod, it's early in the morning and I'm too tired make it properly
+        res = super(Picking, self).action_done()
         for pick in self:
             origin = pick.sale_id and pick.sale_id.sudo().auto_purchase_order_id.origin
-            if origin:
+            if origin and origin.startswith('SO/EEX'):
+                if pick.sale_id.sudo().auto_purchase_order_id.company_id.id != 1:
+                    continue
+
                 sale_origin = self.env['sale.order'].sudo().search([('name', '=', origin)], limit=1)
                 if sale_origin and sale_origin.partner_shipping_id == pick.partner_id:
                     # it's dropshipping
-                    tmp_carrier = pick.carrier_id
-                    pick.carrier_id = False
-                    if not pick.number_of_packages:
-                        pick.number_of_packages = 1
-                    res = super(Picking, self).action_done()
                     pick_origin = sale_origin.picking_ids.filtered(
                         lambda p: p.partner_id.commercial_partner_id == pick.company_id.partner_id
                     )
-                    if pick_origin:
-                        pick_origin = pick_origin[0]
-                        tmp_loc = pick_origin.location_id
-                        tmp_rec = pick_origin.partner_id
-                        pick_origin.location_id = pick.location_id
-                        pick_origin.partner_id = pick.partner_id
-                        pick_origin.number_of_packages = pick.number_of_packages
-                        for l in pick.move_line_ids:
-                            ol = pick_origin.move_line_ids.filtered(lambda l: l.product_id == l.product_id)
-                            if ol:
-                                ol[0].qty_done = l.qty_done
-                        super(Picking, pick_origin).action_done()
-                        pick.carrier_tracking_ref = pick_origin.carrier_tracking_ref
-                        pick_origin.location_id = tmp_loc
-                        pick_origin.partner_id = tmp_rec
+                    origin_count = len(pick_origin)
+                    if origin_count < 1:
+                        raise ValidationError('Source sale order does not have pickings')
+                    if origin_count > 1:
+                        raise ValidationError('One sale order cannot have more than one picking per supplier.')
+                    tmp_loc = pick_origin.location_id
+                    tmp_rec = pick_origin.partner_id
+                    pick_origin.location_id = pick.location_id
+                    pick_origin.partner_id = pick.partner_id
+                    pick_origin.number_of_packages = pick.number_of_packages = pick.number_of_packages or 1
+                    pick_origin.carrier_id = sale_origin.carrier_id
+                    lines = [l for l in pick.move_line_ids]
+                    for ol in pick_origin.move_line_ids:
+                        i = -1
+                        for l in lines:
+                            i += 1
+                            if l.product_id == ol.product_id:
+                                ol.qty_done = l.qty_done
+                                break
+                        if i > -1:
+                            lines.pop(i)
+                        else:
+                            raise ValidationError('Inconsistent order lines between source and current order.')
+                    if len(lines) > 0:
+                        raise ValidationError('Inconsistent order lines between source and current order.')
 
-                    pick.carrier_id = tmp_carrier
-                    return res
-                else:
-                    super(Picking, self).action_done()
-            else:
-                super(Picking, self).action_done()
+                    pick_origin.action_done()
+                    pick.carrier_tracking_ref = pick_origin.carrier_tracking_ref
+                    pick.carrier_id = pick_origin.carrier_id
+                    pick_origin.location_id = tmp_loc
+                    pick_origin.partner_id = tmp_rec
+
+        return res
