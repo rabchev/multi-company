cf98dd5d908ce5ef3c7b55ff0a7172d45ff0caf1
Julian Lubenov <julian.lubenov@plana.solutions>
2020-07-17 00:17:50 +0300

    purchase_sale_inter_company - validation stock moves

--- purchase_sale_inter_company/models/stock.py
+++ purchase_sale_inter_company/models/stock.py
@@ -44,6 +44,12 @@ class Picking(models.Model):
                     if not lines or len(lines) == 0:
                         raise ValidationError('Inconsistent order lines between source and current order.')
                     
+                    for prodID in pick_origin.move_lines.product_id:
+                        pickLen = len(pick.move_lines.filtered(lambda p: p.product_id == prodID))
+                        originLen = len(pick_origin.move_lines.filtered(lambda p: p.product_id == prodID))
+                        if pickLen != originLen:
+                            raise ValidationError('Inconsistent order lines between source and current order.')
+                    
                     self._copy_move_lines(pick_origin, lines)
                     
                     # refering origin SO document
